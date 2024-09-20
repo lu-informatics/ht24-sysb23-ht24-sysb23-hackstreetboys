910b717e915c9ab279079296b2d3025bf78ab5eb
Erik Hellryd <er8263he-s@student.lu.se>
2024-09-20 15:29:45 +0200

    Set up packages for controllers, models and data

--- src/main/java/se/lu/ics/App.java
+++ src/main/java/se/lu/ics/App.java
@@ -9,7 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
 
         // Load connection properties from file

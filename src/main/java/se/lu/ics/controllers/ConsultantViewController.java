a34393232c7be9442a1842dca2a4605701cefcb9
Anna Svelander <an8388sv-s@student.lu.se>
2024-09-25 13:21:51 +0200

    tableView for Consultant details fixed

--- src/main/java/se/lu/ics/controllers/ConsultantViewController.java
+++ src/main/java/se/lu/ics/controllers/ConsultantViewController.java
@@ -1,137 +1,149 @@
-package se.lu.ics.controllers;
+    package se.lu.ics.controllers;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
-import se.lu.ics.data.ConsultantDao;
-import se.lu.ics.data.ProjectDao;
-import se.lu.ics.data.WorkDao;
-import se.lu.ics.models.Consultant;
-import se.lu.ics.models.Project;
-import se.lu.ics.models.Work;
+ 
+    import javafx.beans.property.SimpleStringProperty;
+    import javafx.collections.ObservableList;
+    import javafx.event.ActionEvent;
+    import javafx.fxml.FXML;
+    import javafx.fxml.Initializable;
+    import javafx.scene.control.Button;
+    import javafx.scene.control.TableColumn;
+    import javafx.scene.control.TableView;
+    import javafx.scene.control.cell.PropertyValueFactory;
+    import javafx.scene.layout.Pane;
+    import javafx.scene.text.Text;
+    import se.lu.ics.data.ConsultantDao;
+    import se.lu.ics.data.ProjectDao;
+    import se.lu.ics.data.WorkDao;
+    import se.lu.ics.models.Consultant;
+    import se.lu.ics.models.Project;
+    import se.lu.ics.models.Work;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-
-public class ConsultantViewController implements Initializable {
-
-    private ProjectDao projectDao;
-    private ConsultantDao consultantDao;
-    private WorkDao workDao;
-    private Consultant consultant;
+    import java.net.URL;
+    import java.util.ResourceBundle;
 
 
-          //buttons
-        @FXML
-        private Button btnCloseConsultantDetails;
-    
-        @FXML
-        private Button btnEditConsultant;
-    
-        @FXML
-        private Button btnRegisterConsultantToProject;
-    
-        @FXML
-        private Button btnRemoveConsultantFromProject;
-    
-        //tableview
-        @FXML
-        private TableView<Work> tableViewConsultantProjects;
+    public class ConsultantViewController implements Initializable {
 
-        @FXML
-        private TableColumn<Work, String> tableColumnProjectName;
-    
-        @FXML
-        private TableColumn<Work, String> tableColumnProjectID;
-    
-        @FXML
-        private TableColumn<Work, Integer> tableColumnTotaltHours;
-    
-        @FXML
-        private TableColumn<Work, Integer> tableColumnWeeklyHours;
-    
-        //textfields
-        @FXML
-        private Text textEmployeeNO;
-    
-        @FXML
-        private Text textEmployeeName;
-    
-        @FXML
-        private Text textEmployeeTitle;
-    
-        //Warning Pane
+        private ProjectDao projectDao;
+        private ConsultantDao consultantDao;
+        private WorkDao workDao;
+        private Consultant consultant;
 
-        @FXML
-        private Pane warningPane;
 
-        @FXML
-        void handleBtnCloseConsultantDetails(ActionEvent event) { 
-}
-    
-        @FXML
-        void handleBtnEditConsultant(ActionEvent event) {       
+        
+            //buttons
+            @FXML
+            private Button btnCloseConsultantDetails;
+        
+            @FXML
+            private Button btnEditConsultant;
+        
+            @FXML
+            private Button btnRegisterConsultantToProject;
+        
+            @FXML
+            private Button btnRemoveConsultantFromProject;
+        
+            //tableview
+            @FXML
+            private TableView<Work> tableViewConsultantProjects;
+
+            @FXML
+            private TableColumn<Work, String> tableColumnProjectName;
+        
+            @FXML
+            private TableColumn<Work, String> tableColumnProjectID;
+        
+            @FXML
+            private TableColumn<Work, Integer> tableColumnTotaltHours;
+        
+            @FXML
+            private TableColumn<Work, Integer> tableColumnWeeklyHours;
+        
+            //textfields
+            @FXML
+            private Text textEmployeeNO;
+        
+            @FXML
+            private Text textEmployeeName;
+        
+            @FXML
+            private Text textEmployeeTitle;
+        
+            //Warning Pane
+
+            @FXML
+            private Pane warningPane;
+
+            @FXML
+            void handleBtnCloseConsultantDetails(ActionEvent event) { 
+    }
+        
+            @FXML
+            void handleBtnEditConsultant(ActionEvent event) {       
+            }
+        
+            @FXML
+            void handleBtnRegisterConsultantToProject(ActionEvent event) {    
+            }
+        
+            @FXML
+            void handleBtnRemoveFromProject(ActionEvent event) {    
+            }
+
+
+            @Override
+            public void initialize(URL location, ResourceBundle resources) {
+                intitaizeDaos();
         }
-    
-        @FXML
-        void handleBtnRegisterConsultantToProject(ActionEvent event) {    
-        }
-    
-        @FXML
-        void handleBtnRemoveFromProject(ActionEvent event) {    
-        }
 
+            public void setConsultant(Consultant consultant) {
+                this.consultant = consultant;
+                if (consultant !=null){
+                    setupWorkTableView();
+                } else {
+                    System.out.println("Consultant is null. Cannot setup work table view.");
+                }
+            }
+                
+                private void intitaizeDaos(){
+                    try {
+                        consultantDao = new ConsultantDao();
+                        projectDao = new ProjectDao();
+                        workDao = new WorkDao();
+                        
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }   
+                }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+            // setup worktable view method
+                private void setupWorkTableView() {
+                    
+                
+                            tableColumnProjectName.setCellValueFactory(cellData -> {
+                    Work work = cellData.getValue();
+                    return new SimpleStringProperty(work.getProject().getProjectName());
+                });
 
-        try {
-            consultantDao = new ConsultantDao();
-            projectDao = new ProjectDao();
-            workDao = new WorkDao();
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }   
-
-       
-        // Initialize the TableView columns
-        //    tableColumnProjectName.setCellValueFactory(cellData -> 
-        //     new SimpleStringProperty(cellData.getValue().getProject().getProjectName()));
-        //     tableColumnProjectID.setCellValueFactory(cellData -> 
-        //     new SimpleStringProperty(cellData.getValue().getProject().getProjectNo()));
-
-        //     tableColumnTotaltHours.setCellValueFactory(new PropertyValueFactory<>("hoursWorked"));
-        //     tableColumnWeeklyHours.setCellValueFactory(new PropertyValueFactory<>("weeklyHours"));
-
-        // // Load data into the TableView
-        // // if (workDao != null && consultant.getEmployeeNo() != null) {
-        //     ObservableList<Work> workDetails = workDao.findConsultantByEmployeeNo(consultant.getEmployeeNo());
-        //     tableViewConsultantProjects.setItems(workDetails);
-        // }
-}
-
-     public void setConsultant(Consultant consultant) {
-         this.consultant = consultant;
-     }
-
-    //     // Populate text fields with consultant details
-    //     textEmployeeNO.setText(consultant.getEmployeeNo());
-    //     textEmployeeName.setText(consultant.getEmployeeName());
-    //     textEmployeeTitle.setText(consultant.getEmployeeTitle());
-
-
-    //     // Load and populate table with project details
-    //     ObservableList<Work> workDetails = workDao.findConsultantByEmployeeNo(employeeNo);
-    //     tableViewConsultantProjects.setItems(workDetails);
-    // }
-}
+                tableColumnProjectID.setCellValueFactory(cellData -> {
+                    Work work = cellData.getValue();
+                    return new SimpleStringProperty(work.getProject().getProjectNo());
+                });
+                    tableColumnTotaltHours.setCellValueFactory(new PropertyValueFactory<>("hoursWorked"));
+                    tableColumnWeeklyHours.setCellValueFactory(new PropertyValueFactory<>("weeklyHours"));
+                
+                    // Debug statement to check consultant
+                    System.out.println("Consultant: " + consultant);
+                
+                    // Use workdao method findConsultantByEmployeeNo to get the list of work
+                    ObservableList<Work> workList = workDao.findConsultantByEmployeeNo(consultant.getEmployeeNo());
+                
+                    // Debug statement to check workList
+                    System.out.println("Work List: " + workList);
+                
+                    // Set the list of work to the tableview
+                    tableViewConsultantProjects.setItems(workList);
+                }
+                }

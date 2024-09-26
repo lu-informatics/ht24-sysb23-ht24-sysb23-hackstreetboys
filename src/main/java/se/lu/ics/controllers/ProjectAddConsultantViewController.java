c2239c7410c0eaad2f40a4a97c74a6c6b26e0465
Erik Hellryd <er8263he-s@student.lu.se>
2024-09-26 15:32:15 +0200

    Refactor ConsultantDetailsEditViewController and ProjectAddConsultantViewController
    Remove unused variables and imports in ConsultantDetailsEditViewController.
    Clean up imports and remove unused variables in ProjectAddConsultantViewController.

--- src/main/java/se/lu/ics/controllers/ProjectAddConsultantViewController.java
+++ src/main/java/se/lu/ics/controllers/ProjectAddConsultantViewController.java
@@ -1,50 +1,50 @@
 package se.lu.ics.controllers;
 
-import java.io.IOException;
 import java.net.URL;
+import javafx.util.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import se.lu.ics.data.ConsultantDao;
+import se.lu.ics.data.ProjectDao;
 import se.lu.ics.data.WorkDao;
 import se.lu.ics.models.Consultant;
 import se.lu.ics.models.Project;
-
 
 public class ProjectAddConsultantViewController implements Initializable {
 
     private ConsultantDao consultantDao;
     private ProjectViewController projectViewController;
-    private WorkDao workDao;
+
     private Project project;
 
+    // Buttons
     @FXML
     private Button btnAddToProject;
 
     @FXML
     private Button btnClose;
 
-    @FXML
-    private Pane paneWarning;
-
+    // TableColumns
     @FXML
     private TableColumn<Consultant, String> tableColumnConsultantId;
 
@@ -66,74 +66,32 @@ public class ProjectAddConsultantViewController implements Initializable {
     @FXML
     private TableView<Consultant> tableViewConsultants;
 
+    // Text
     @FXML
-    private Text textEmployeeNO;
-
-    @FXML
-    private Text textEmployeeName;
+    private Text textProjectNO;
 
     @FXML
-    private Text textEmployeeTitle;
+    private Text textProjectName;
+
+    // Pane
+    @FXML
+     private Pane paneWarning;
 
     @FXML
-    void handleBtnAddToProject(ActionEvent event) {
-        Consultant consultant = tableViewConsultants.getSelectionModel().getSelectedItem();
-        if (consultant == null) {
-            showWarningMessage("No consultant selected!");
-            return;
-        }
-    
-        if (project == null) {
-            showWarningMessage("Project is not initialized!");
-            return;
-        }
-    
-        String projectNo = project.getProjectNo(); // Use the project object to get the project number
-        String employeeNo = consultant.getEmployeeNo();
-        int hoursWorked = 0; // Set appropriate value
-        int weeklyHours = 0; // Set appropriate value
-    
-        WorkDao workDao;
-        try {
-            workDao = new WorkDao();
-            workDao.addConsultantToProject(projectNo, employeeNo, hoursWorked, weeklyHours);
-            showSuccessMessage("Consultant added to project successfully!");
-        } catch (Exception e) {
-            showWarningMessage("Could not add consultant to project!");
-            e.printStackTrace();
-        
-        }
-        projectViewController.updateTableView();
-        
-    }
+    private Label labelWarning;
 
-
-
-    
-
-    @FXML
-    void handleBtnClose(ActionEvent event) {
-           // Get the current stage and close it
-
-        Stage stage = (Stage) btnClose.getScene().getWindow();
-        stage.close();
-    }
-
-    @FXML
-    void handlePaneWarning(MouseEvent event) {
-        paneWarning.setVisible(false);
-    }
+    // INITALIZE WINDOW
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initializeDaos();
-        setupConsultantsTableView();  
-        ;
+        setupConsultantsTableView();
     }
 
     private void initializeDaos() {
         try {
             consultantDao = new ConsultantDao();
+            projectDao = new ProjectDao();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -182,6 +140,8 @@ public class ProjectAddConsultantViewController implements Initializable {
         List<Consultant> consultants = consultantDao.findAllConsultants();
         ObservableList<Consultant> observableConsultants = FXCollections.observableArrayList(consultants);
         tableViewConsultants.setItems(observableConsultants);
+
+        
     }
 
     // Update consultant table view
@@ -194,40 +154,120 @@ public class ProjectAddConsultantViewController implements Initializable {
         setupConsultantsTableView();
     }
 
+        // Populate project details in Text fields
+        private void populateProjectDetails(String projectNo) {
+            project = projectDao.findByProjectNo(projectNo);
+            if (project != null) {
+                textProjectNO.setText(project.getProjectNo());
+                textProjectName.setText(project.getProjectName());
+            } else {
+                setWarning("Project not found!");
+            }
+        }
+
+
+
+    // BUTTON HANDLERS
+    @FXML
+    void handleBtnAddToProject(ActionEvent event) {
+        Consultant consultant = tableViewConsultants.getSelectionModel().getSelectedItem();
+        if (consultant == null) {
+            setWarning("No consultant selected!");
+            return;
+        }
+
+        if (project == null) {
+            setWarning("Project is not initialized!");
+            return;
+        }
+
+        String projectNo = project.getProjectNo(); // Use the project object to get the project number
+        String employeeNo = consultant.getEmployeeNo();
+        int hoursWorked = 0; // Set appropriate value
+        int weeklyHours = 0; // Set appropriate value
+
+        WorkDao workDao;
+        try {
+            workDao = new WorkDao();
+            workDao.addConsultantToProject(projectNo, employeeNo, hoursWorked, weeklyHours);
+            showSuccessMessage("Consultant added to project successfully!");
+        } catch (Exception e) {
+            setWarning("Could not add consultant to project!");
+            e.printStackTrace();
+
+        }
+        projectViewController.updateTableView();
+        mainViewController.updateProjectsTableView();
+
+    }
+
+    @FXML
+    void handleBtnClose(ActionEvent event) {
+        // Get the current stage and close it
+
+        Stage stage = (Stage) btnClose.getScene().getWindow();
+        stage.close();
+    }
+
+    // Pane warning
+    @FXML
+    void handlePaneWarning(MouseEvent event) {
+        paneWarning.setVisible(false);
+    }
+
+    @FXML
+    void handleLabelWarning(MouseEvent event) {
+        labelWarning.setVisible(false);
+    }
+
+    // SET AND GET METHODS FOR INSTANCES
+
     public void setProjectViewController(ProjectViewController projectViewController) {
         this.projectViewController = projectViewController;
     }
 
+    public void setMainViewController(MainViewController mainViewController) {
+        this.mainViewController = mainViewController;
+    }
+
     public void setProject(Project project) {
         this.project = project;
+        if (project != null) {
+            populateProjectDetails(project.getProjectNo());
+        }
     }
 
-    private void showSuccessMessage(String message) {
+    // SUCCESS AND WARNING MESSAGES
+
+    public void showSuccessMessage(String message) {
         paneWarning.setStyle("-fx-background-color: green;");
         paneWarning.setVisible(true);
-        textEmployeeNO.setText(message);
-        new Thread(() -> {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            paneWarning.setVisible(false);
-        }).start();
+        labelWarning.setText(message);
+
+        Timeline timeline = new Timeline(new KeyFrame(
+                Duration.seconds(5),
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        paneWarning.setVisible(false);
+                    }
+                }));
+        timeline.play();
     }
 
-    private void showWarningMessage(String message) {
+    public void setWarning(String message) {
         paneWarning.setStyle("-fx-background-color: red;");
         paneWarning.setVisible(true);
-        textEmployeeNO.setText(message);
-        new Thread(() -> {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            paneWarning.setVisible(false);
-        }).start();
-    }
+        labelWarning.setText(message);
 
+        Timeline timeline = new Timeline(new KeyFrame(
+                Duration.seconds(5),
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        paneWarning.setVisible(false);
+                    }
+                }));
+        timeline.play();
+    }
 }
--- src/main/java/se/lu/ics/controllers/ProjectViewController.java
+++ src/main/java/se/lu/ics/controllers/ProjectViewController.java
@@ -6,10 +6,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Labeled;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -18,12 +16,9 @@ import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import se.lu.ics.data.ConsultantDao;
-import se.lu.ics.data.ProjectDao;
 import se.lu.ics.data.WorkDao;
 import se.lu.ics.data.MilestoneDao;
 import javafx.beans.property.SimpleIntegerProperty;
-
-
 
 //import model classes
 import se.lu.ics.models.Consultant;
@@ -33,20 +28,17 @@ import se.lu.ics.models.Work;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 
-public class ProjectViewController implements Initializable{
+public class ProjectViewController implements Initializable {
 
     private Project project;
     private ConsultantDao consultantDao;
-    private ProjectDao projectDao;
     private MilestoneDao milestoneDao;
     private MainViewController mainViewController;
-
 
     // A setter method for MainViewController
     public void setMainViewController(MainViewController mainViewController) {
@@ -70,8 +62,6 @@ public class ProjectViewController implements Initializable{
 
     @FXML
     private Button btnRemoveMilestone;
-
-   
 
     @FXML
     private TableColumn<Milestone, LocalDate> tableColumnDate;
@@ -106,11 +96,9 @@ public class ProjectViewController implements Initializable{
     @FXML
     private Pane warningPaneProjectView;
 
-
-
-
     @FXML
-    void handleBtnAddConsultant(ActionEvent event) {       try {
+    void handleBtnAddConsultant(ActionEvent event) {
+        try {
             // Load the FXML file for the ProjectAddConsultantView
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/ProjectAddConsultantView.fxml"));
             Pane ProjectAddConsultantViewPane = loader.load();
@@ -122,6 +110,7 @@ public class ProjectViewController implements Initializable{
             //Passes the projectobject to ProjectAddConsultantViewController
             ProjectAddConsultantcontroller.setProject(this.project);
             ProjectAddConsultantcontroller.setProjectViewController(this);
+            ProjectAddConsultantcontroller.setMainViewController(mainViewController);
             
 
             // Create a new stage for the window
@@ -140,11 +129,37 @@ public class ProjectViewController implements Initializable{
         }
 
     }
+// Opens MilestoneAdd.fxml
+@FXML
+public void handleBtnAddMilestone(ActionEvent event) {
+    try {
+        // Load the FXML file for the MilestoneAdd view
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MilestoneAdd.fxml"));
+        Parent root = loader.load();
 
-    @FXML
-    void handleBtnAddMilestone(ActionEvent event) {
+        // Get the controller for the loaded FXML
+        MilestoneAddController milestoneAddController = loader.getController();
 
+        // Set the selected project in the MilestoneAddController
+        milestoneAddController.setProject(project);  // 'project' är det aktuella projektet
+
+        // Create a new stage for the window
+        Stage modalStage = new Stage();
+        modalStage.setScene(new Scene(root));
+        modalStage.setTitle("Add Milestone");
+
+        // Set the stage to be modal
+        modalStage.initModality(Modality.APPLICATION_MODAL);
+
+        // Show the stage and wait for it to close
+        modalStage.showAndWait();
+    } catch (IOException e) {
+        displayErrorMessage("Could not open the add milestone view, contact support");
+        e.printStackTrace();
     }
+}
+
+
 
     @FXML
     void handleBtnClose(ActionEvent event) {
@@ -158,6 +173,7 @@ public class ProjectViewController implements Initializable{
     void handleBtnEditProjectInfo(ActionEvent event) {
 
     }
+
     @FXML
     void handleBtnRemoveConsultant(ActionEvent event) {
         // Get the selected consultant from the TableView
@@ -179,10 +195,10 @@ public class ProjectViewController implements Initializable{
                 loadConsultant();
                 loadMilestones();
 
-               // Update the projects table view in MainViewController
-               if (mainViewController != null) {
-                mainViewController.updateProjectsTableView();
-            }
+                // Update the projects table view in MainViewController
+                if (mainViewController != null) {
+                    mainViewController.updateProjectsTableView();
+                }
 
             } catch (Exception e) {
                 // Handle exception, show error message or log the error
@@ -195,15 +211,45 @@ public class ProjectViewController implements Initializable{
         }
     }
 
-    @FXML
-    void handleBtnRemoveMilestone(ActionEvent event) {
+    //Method for removing a milestone from a project
+        @FXML
+        void handleBtnRemoveMilestone(ActionEvent event) {
+            // Get the selected milestone from the TableView
+            Milestone selectedMilestone = tableViewMilestoneInfo.getSelectionModel().getSelectedItem();
+    
+            if (selectedMilestone != null) {
+                try {
+                    // Call the DAO method to remove the milestone from the project
+                    milestoneDao.deleteMilestone(selectedMilestone, project);
+    
+                    // Show a success message (optional)
+                    System.out.println("Milestone removed from project successfully.");
+    
+                    // Refresh the ProjectView to reflect changes
+                    loadConsultant();
+                    loadMilestones();
+    
+                    // Update the projects table view in MainViewController
+                    if (mainViewController != null) {
+                        mainViewController.updateProjectsTableView();
+                    }
+    
+                } catch (Exception e) {
+                    // Handle exception, show error message or log the error
+                    displayErrorMessage("Error occurred while removing milestone from project: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            } else {
+                // If no milestone is selected, show an error message
+                displayErrorMessage("Please select a milestone to remove.");
+            }
 
     }
 
     public void setProject(Project project) {
-       this.project = project;
-       loadConsultant();
-       loadMilestones();
+        this.project = project;
+        loadConsultant();
+        loadMilestones();
     }
 
     @Override
@@ -244,8 +290,7 @@ public class ProjectViewController implements Initializable{
         tableColumnDescription.setCellValueFactory(new PropertyValueFactory<>("milestoneDescription"));
     }
 
-
-    //LOAD CONSULTANT
+    // LOAD CONSULTANT
     private void loadConsultant() {
         clearErrorMessage();
         project = ProjectViewController.this.project;
@@ -274,7 +319,7 @@ public class ProjectViewController implements Initializable{
         }
     }
 
-    //LOAD MILESTONES
+    // LOAD MILESTONES
     private void loadMilestones() {
         clearErrorMessage();
         project = ProjectViewController.this.project;
@@ -314,5 +359,9 @@ public class ProjectViewController implements Initializable{
         throw new UnsupportedOperationException("Unimplemented method 'getProjectNo'");
     }
     
+   public void setmainViewController(MainViewController mainViewController) {
+       this.mainViewController = mainViewController;
+   }
+
 
 }
